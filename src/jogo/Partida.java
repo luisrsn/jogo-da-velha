4e052c14b8873d6faf9192ba105ded18ac45a633
Luis Ribeiro <luisrsn@gmail.com>
2020-07-27 18:36:38 -0300

    TabuleiroException e programação defensiva

--- src/jogo/Partida.java
+++ src/jogo/Partida.java
@@ -8,14 +8,14 @@ public class Partida {
 	private Tabuleiro tabuleiro;
 	
 	public Partida() {
-		tabuleiro = new Tabuleiro(3, 3);
+		tabuleiro = new Tabuleiro();
 		configuracaoInicial();
 	}
 	
 	public PecaDoJogo[][] getPecas() {
-		PecaDoJogo[][] mat = new PecaDoJogo[tabuleiro.getLinha()][tabuleiro.getColuna()];
-		for(int i = 0; i < tabuleiro.getLinha(); i ++) {
-			for(int j = 0; j < tabuleiro.getColuna(); j ++) {
+		PecaDoJogo[][] mat = new PecaDoJogo[tabuleiro.getLinhas()][tabuleiro.getColunas()];
+		for(int i = 0; i < tabuleiro.getLinhas(); i ++) {
+			for(int j = 0; j < tabuleiro.getColunas(); j ++) {
 				mat[i][j] = (PecaDoJogo) tabuleiro.peca(i, j);
 			}
 		}
@@ -23,6 +23,6 @@ public class Partida {
 	}
 	
 	private void configuracaoInicial() {
-		tabuleiro.colocarPeca(new PecaDoJogo(tabuleiro, Valor.X), new Posicao(2, 1));
+		tabuleiro.colocarUmaPeca(new PecaDoJogo(tabuleiro, Valor.X), new Posicao(2, 1));
 	}
 }
--- src/tabuleiro/Tabuleiro.java
+++ src/tabuleiro/Tabuleiro.java
@@ -2,46 +2,56 @@ package tabuleiro;
 
 public class Tabuleiro {
 	
-	private int linha;
-	private int coluna;
+	private int linhas = 3;
+	private int colunas = 3;
 	private Peca[][] pecas;
-
-//	public Tabuleiro() {
-//		imprimirTabuleiro();
-//	}
 	
-	public Tabuleiro(int linha, int coluna) {
-		this.linha = linha;
-		this.coluna = coluna;
-		pecas = new Peca[linha][coluna];
+	public Tabuleiro() {
+		pecas = new Peca[linhas][colunas];
 	}		
 
-	public int getLinha() {
-		return linha;
+	public int getLinhas() {
+		return linhas;
 	}
 
-	public void setLinha(int linha) {
-		this.linha = linha;
-	}
-
-	public int getColuna() {
-		return coluna;
-	}
-
-	public void setColuna(int coluna) {
-		this.coluna = coluna;
+	public int getColunas() {
+		return colunas;
 	}
 
 	public Peca peca(int linha, int coluna) {
+		if(!posicaoExistente(linha, coluna)) {
+			throw new TabuleiroException("Posição não existente no jogo");
+		}
 		return pecas[linha][coluna];
 	}
 	
 	public Peca peca(Posicao posicao) {
+		if(!posicaoExistente(posicao)) {
+			throw new TabuleiroException("Posição não existente no jogo");
+		}
 		return pecas[posicao.getLinha()][posicao.getColuna()];
 	}	
 	
-	public void colocarPeca(Peca peca, Posicao posicao) {
+	public void colocarUmaPeca(Peca peca, Posicao posicao) {
+		if(temUmaPeca(posicao)) {
+			throw new TabuleiroException("Já existe uma peça na posicação " + posicao);
+		}
 		pecas[posicao.getLinha()][posicao.getColuna()] = peca;
 		peca.posicao = posicao;
+	}
+	
+	private boolean posicaoExistente(int linha, int coluna) {
+		return linha >= 0 && linha < linhas && coluna >= 0 && coluna < colunas;
+	}
+	
+	public boolean posicaoExistente(Posicao posicao) {
+		return posicaoExistente(posicao.getLinha(), posicao.getColuna());
+	}
+	
+	public boolean temUmaPeca(Posicao posicao) {
+		if(!posicaoExistente(posicao)) {
+			throw new TabuleiroException("Posição não existente no jogo");
+		}
+		return peca(posicao) != null;
 	}
 }
